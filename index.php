a5e72766f6f6fa0fc3bbfcac8f10cd95dc615b7d
realyussel <yussel.br@gmail.com>
2022-05-14 16:01:57 -0500

    cambios menores

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ set_time_limit(20);
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="dist/notas.css">
 </head>
-<body>
+<body class="container">
 
 <?php
 /*
